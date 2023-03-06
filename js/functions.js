b86c6578bbcea30015103309769b1294c892bc0b
ertelicious <veronika.ertel@gmail.com>
2023-03-06 11:38:04 +0200

    function Palindrome

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,7 @@
 
 let stringSentence = 'проверяемая строка';
 
+
 // Cтрока короче 20 символов
 console.log(stringSentence.length, 20); // true
 
@@ -10,3 +11,45 @@ console.log(stringSentence.length, 18); // true
 
 // Строка длиннее 10 символов
 console.log(stringSentence.length, 10); // true
+
+
+
+// 2. Палиндром
+
+// Строка является палиндромом. имяФункции('топот'); // true
+
+function isPalindrome(string){
+  for (let i = 0, j = string.length - 1; i < j; i++, j--){
+    if(string[i] != string[j])
+    return false;
+  }
+  return true;
+}
+let string = 'топот';
+
+console.log(isPalindrome(string));
+
+// Несмотря на разный регистр, тоже палиндром. имяФункции('ДовОд'); // true
+
+function isPalindrome(string){
+  for (let i = 0, j = string.length - 1; i < j; i++, j--){
+    if(string[i] != string[j])
+    return false;
+  }
+  return true;
+}
+let string = 'ДовОд';
+console.log(isPalindrome(string.toLowerCase));
+
+// Это не палиндром. имяФункции('Кекс');  // false
+
+function isPalindrome(string){
+  for (let i = 0, j = string.length - 1; i < j; i++, j--){
+    if(string[i] != string[j])
+    return false;
+  }
+  return true;
+}
+let string = 'Кекс';
+string.toLowerCase();
+console.log(isPalindrome(string));
