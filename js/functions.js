0ad0082d91796e5ed42265010955c4b5044878b1
ertelicious <veronika.ertel@gmail.com>
2023-03-06 10:16:17 +0200

    function length

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+// 1. Функция для проверки длины строки
+
+let stringSentence = 'проверяемая строка';
+
+// Cтрока короче 20 символов
+console.log(stringSentence.length, 20); // true
+
+// Длина строки ровно 18 символов
+console.log(stringSentence.length, 18); // true
+
+// Строка длиннее 10 символов
+console.log(stringSentence.length, 10); // true
